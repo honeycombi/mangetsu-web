e0326149856c818329af2c7402a0a9dc870206be
honeycombi <garyhu06@gmail.com>
2022-09-25 14:59:31 -0400

    update

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,7 +1,7 @@
 export default function SectionContainer({ children }) {
   return (
     <div>
-      <div className="layoutbg block dark:hidden">
+      <div className="layoutbg dark:hidden">
         <div className=" mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
       </div>
       <div className="hidden dark:block dark:bg-gray-800">
